79659dced95e1e79e00930d2bd29a8e0ab5c55d1
Dmytro Kravchenko <44780251+sys-dev-me@users.noreply.github.com>
2018-11-28 17:12:22 +0200

    Add files via upload

---
+++ pw-server.go
@@ -0,0 +1,82 @@
+package main
+
+
+import "log"
+import "fmt"
+import "os"
+import "net"
+import "bufio"
+import "math/rand"
+import "strconv"
+import "strings"
+
+const (
+	CONN_TYPE = "tcp"
+)
+
+
+var pChar ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0987654321{}()$-=!@#_/\\_&"
+
+func generatePassword ( pwSize int ) (string) {
+	src := []rune(pChar)
+	res := make([]rune, pwSize)
+
+	for i := range res {
+        res[i] = src[rand.Intn(len(src)-1)]
+    }
+
+	return string( res )
+}
+
+func handleConnection ( conn net.Conn ) {
+
+	defer conn.Close()
+	rw := bufio.NewReadWriter(bufio.NewReader(conn), bufio.NewWriter(conn))
+
+	for {
+        req, err := rw.ReadString('\n')
+        if err != nil {
+            rw.WriteString("failed to read input")
+            rw.Flush()
+            return
+        }
+		
+		req = strings.Trim( req, "\n" )	
+		pwLen, err := strconv.Atoi( req )
+		if err !=nil {
+			rw.WriteString( "Wrong data")
+			return 
+		}
+
+        rw.WriteString( fmt.Sprintf( generatePassword( pwLen ) ) + "\n" )
+        rw.Flush()
+    }
+
+}
+
+func main () {
+
+	if len( os.Args ) < 2 {
+		log.Println( "Not enough arguments. Usage example: pw-server [port]" ) 
+		os.Exit( 1 )
+	}
+	
+	port := os.Args[1]
+	conn, err := net.Listen(CONN_TYPE, "localhost:"+port)
+
+	// check if not listening
+	if err !=nil {
+		log.Println( "Unable to bind on port ", port )
+	}
+
+	log.Println( "Listening on: ", port )
+	for {
+		 res, err := conn.Accept()
+		 if err !=nil {
+		 	log.Println( "Something went wrong, can't accept connection" )
+		 }
+
+		go handleConnection(res)
+		defer conn.Close()
+	}
+}
